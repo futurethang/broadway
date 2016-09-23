71b2c4e94f1f89e00382b7c49d3790713bd7cb8c
Kevin <kphyde@gmail.com>
2016-09-23 11:09:47 -0700

    add new and create actions with view files and form patial

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -3,5 +3,19 @@ class PlaysController < ApplicationController
 
   end
 
-  
+  def new #a view action
+    @play = Play.new
+  end
+
+  def create #a database action
+    @play = Play.new(play_params)
+  end
+
+
+  private
+
+  def play_params
+    params.require(:play).permit(:title, :description, :director)
+  end
+
 end
