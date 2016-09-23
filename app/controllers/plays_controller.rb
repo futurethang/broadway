72c132de2cf88eb86939858108b4e8abfdbe54e1
Kevin <kphyde@gmail.com>
2016-09-23 12:43:35 -0700

    add nav and user model

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -9,11 +9,11 @@ class PlaysController < ApplicationController
   end
 
   def new #a view action
-    @play = Play.new
+    @play = current_user.play.build
   end
 
   def create #a database action
-    @play = Play.new(play_params)
+    @play = current_user.play.build(play_params)
 
     if @play.save
       redirect_to root_path
